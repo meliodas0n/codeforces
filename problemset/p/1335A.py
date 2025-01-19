fb15c0131397ecdc3d50a2a6f6f0436d5e1786c7
meliodas0n <mrunalnarayana@gmail.com>
2025-01-19 14:16:44 +0000

    1335A

--- problemset/p/1335A.py
+++ problemset/p/1335A.py
@@ -1,9 +1,13 @@
-
 def main(inp):
-  return
+  for candles in inp:
+    if candles >= 2 * 1e9:
+      print(int(1e9 - 1))
+    elif candles < 3:
+      print(0)
+    else:
+      print(int((candles / 2) - 1) if candles % 2 == 0 else int(candles // 2))
 
 if __name__ == '__main__':
   n = int(input())
-  inputs = [input() for _ in range(n)]
-  print(main(inputs))
-
+  inputs = [int(input()) for _ in range(n)]
+  main(inputs)
